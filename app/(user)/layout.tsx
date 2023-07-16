e4adabc2098e651b15973a7c3dfad913df3c5f52
ahsan <makabir98@yahoo.com>
2023-07-16 07:30:02 +0600

    frontend: banner done

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -2,6 +2,7 @@ import '../globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Header from '../components/Header'
+import Banner from '../components/Banner'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Header />
-        {/* Banner */}
+        <Banner />
         {children}
       </body>
     </html>
--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -3,8 +3,6 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div>
-      <h1 className='text-4xl'>Welcome to Ahsan&apos;s blog</h1>
-      <p>Some content</p>
     </div>
   )
 }
